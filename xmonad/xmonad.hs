b6db403a7eb768bdfca036dc8a2ee0f33271a2e4
HokieGeek <andres.f.perez@gmail.com>
2013-11-04 20:20:33 -0500

    Making more and more progress on configuring xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -5,11 +5,11 @@ import XMonad.Actions.CycleWS
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
 import qualified XMonad.StackSet as W
 --}}}
 
-myWorkspaces = ["1","2","3","4","5","6","7","8","9","0","-","="]
+-- Hooks {{{
+-- Manage Hooks {{{
 myManageHook = composeAll
     [ className =? "Gimp"                           --> doFloat
     , className =? "VASSAL-launch-ModuleManager"    --> doFloat
@@ -19,19 +19,28 @@ myManageHook = composeAll
     , isFullscreen --> doFullFloat
     ]
     where viewShift = doF . liftM2 (.) W.greedyView W.shift
+-- }}}
+-- }}}
+
+myWorkspaces = ["1","2","3","4","5","6","7","8","9","0","-","="]
 
 -- Main {{{
 main = do
     xmonad $ defaultConfig
         { workspaces = myWorkspaces
+        , modMask = mod4Mask
         , manageHook = myManageHook <+> manageHook defaultConfig
         , layoutHook = avoidStruts $ layoutHook defaultConfig
         } `additionalKeys`
-        [ ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        [ (((mod4Mask .|. controlMask), xK_Left), prevWS)
+        , (((mod4Mask .|. controlMask), xK_Right), nextWS)
+        , (((mod4Mask .|. shiftMask), xK_Left), shiftToPrev)
+        , (((mod4Mask .|. shiftMask), xK_Right), shiftToNext)
+        , (((mod4Mask .|. controlMask .|. shiftMask), xK_Down), moveTo Next EmptyWS)
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
-        -- , ((modMask .|. controlMask, xK_Left), prevWS)
-        -- , ((modMask .|. controlMask .|. shiftMask), xK_Right), moveTo Next EmptyWS)
-        -- , ((modMask .|. controlMask, xK_Right), nextWS)
-        -- , ((modMask, xK_Down), nextWS)
+        , ((mod4Mask .|. shiftMask, xK_S), spawn "sudo /usr/sbin/pm-suspend")
+        , (((mod4Mask .|. controlMask .|. shiftMask), xK_Left), spawn "xbacklight -inc 20")
+        , (((mod4Mask .|. controlMask .|. shiftMask), xK_Right), spawn "xbacklight -dec 20")
         ]
 --}}}
