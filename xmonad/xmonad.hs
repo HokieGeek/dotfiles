d4b89757cfd46c7f8d12d3aaf24d72bdea3fd3dd
HokieGeek <andres.f.perez@gmail.com>
2013-11-10 09:19:32 -0500

    It wasn't exactly what I wanted, but I did get keybindings for the new workspaces working

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -18,6 +18,10 @@ import XMonad.Util.EZConfig
 import qualified XMonad.StackSet as W
 --}}}
 
+-- Local Variables {{{
+myWorkspaces = ["1","2","3","4","5","6","7","8","9","0","-","="]
+--}}}
+
 -- Hooks {{{
 -- Manage Hooks {{{
 myManageHook = composeAll
@@ -47,8 +51,40 @@ myLogHook h = dynamicLogWithPP $ defaultPP
 -- }}}
 -- }}}
 
--- TODO: need to define the keybindings!!
-myWorkspaces = ["1","2","3","4","5","6","7","8","9","0","-","="]
+-- Keybindings {{{
+myKeys =
+            [ (((mod4Mask .|. mod1Mask), xK_Left), prevWS)
+            , (((mod4Mask .|. mod1Mask), xK_Right), nextWS)
+            , (((mod4Mask .|. shiftMask), xK_Left), shiftToPrev)
+            , (((mod4Mask .|. shiftMask), xK_Right), shiftToNext)
+            , (((mod4Mask .|. mod1Mask), xK_Up), moveTo Next EmptyWS)
+            , (((mod4Mask .|. mod1Mask), xK_Down), toggleWS)
+            -- Added 3 workspaces (0,-,=)
+            , (((mod4Mask), xK_0), windows $ W.greedyView "0")
+            , (((mod4Mask .|. shiftMask), xK_0), windows $ W.shift "0")
+            , (((mod4Mask), xK_minus), windows $ W.greedyView "-")
+            , (((mod4Mask .|. shiftMask), xK_minus), windows $ W.shift "-")
+            , (((mod4Mask), xK_equal), windows $ W.greedyView "=")
+            , (((mod4Mask .|. shiftMask), xK_equal), windows $ W.shift "=")
+            -- Backlight
+            , (((mod4Mask .|. controlMask .|. shiftMask), xK_Left), spawn "xbacklight -inc 20")
+            , (((mod4Mask .|. controlMask .|. shiftMask), xK_Right), spawn "xbacklight -dec 20")
+            -- Volume
+            -- XF86AudioRaiseVolume
+            , ((0, 0x1008FF13), spawn "amixer -q set Master unmute ; amixer -q set Speaker unmute ; amixer -q set Headphone unmute ; amixer -q set Master playback 3+")
+            -- XF86AudioLowerVolume
+            , ((0, 0x1008FF11), spawn "amixer -q set Master unmute ; amixer -q set Speaker unmute ; amixer -q set Headphone unmute ; amixer -q set Master playback 3-")
+            -- XF86AudioMute
+            , ((0, 0x1008FF12), spawn "amixer -q set Master toggle; amixer -q set Speaker toggle; amixer -q set Headphone toggle")
+            -- PrintScreen
+            , ((0, xK_Print), spawn "scrot")
+            , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+            ]
+            -- ++
+            -- [((m .|. mod4Mask, k), windows $ f i)
+                -- | (i, k) <- zip myWorkspaces [xK_1..xK_9]-- ++ [xK_0]
+                -- , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+-- }}}
 
 -- Main {{{
 workspaceStatusBar = "sleep 3s; dzen2 -x '1440' -y '0' -h '16' -w '270' -fg '#FFFFFF' -bg '#1B1D1E'"
@@ -63,27 +99,5 @@ main = do
             , layoutHook = avoidStruts . smartBorders $ layoutHook defaultConfig
             , logHook = myLogHook dzenLeftBar >> fadeInactiveLogHook 0xdddddddd
             , borderWidth = 0
-            } `additionalKeys`
-            [ (((mod4Mask .|. mod1Mask), xK_Left), prevWS)
-            , (((mod4Mask .|. mod1Mask), xK_Right), nextWS)
-            , (((mod4Mask .|. shiftMask), xK_Left), shiftToPrev)
-            , (((mod4Mask .|. shiftMask), xK_Right), shiftToNext)
-            , (((mod4Mask .|. mod1Mask), xK_Up), moveTo Next EmptyWS)
-            , (((mod4Mask .|. mod1Mask), xK_Down), toggleWS)
-            -- , (((mod1Mask), xK_0), W.greedyView "0")
-            , (((mod4Mask .|. controlMask .|. shiftMask), xK_Left), spawn "xbacklight -inc 20")
-            , (((mod4Mask .|. controlMask .|. shiftMask), xK_Right), spawn "xbacklight -dec 20")
-            -- XF86AudioRaiseVolume
-            , ((0, 0x1008FF13), spawn "amixer -q set Master unmute ; amixer -q set Speaker unmute ; amixer -q set Headphone unmute ; amixer -q set Master playback 3+")
-            -- XF86AudioLowerVolume
-            , ((0, 0x1008FF11), spawn "amixer -q set Master unmute ; amixer -q set Speaker unmute ; amixer -q set Headphone unmute ; amixer -q set Master playback 3-")
-            -- XF86AudioMute
-            , ((0, 0x1008FF12), spawn "amixer -q set Master toggle; amixer -q set Speaker toggle; amixer -q set Headphone toggle")
-            , ((0, xK_Print), spawn "scrot")
-            , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-            ]
-            -- ++
-            -- [((m .|. modMask, k), windows $ f i)
-                -- | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-                -- , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+            } `additionalKeys` myKeys
 --}}}
