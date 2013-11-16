76b4634dd18faa89768533a0f3cdb7e52b1a6cd1
HokieGeek <andres.f.perez@gmail.com>
2013-11-15 23:39:25 -0500

    Modifying setup.sh so that I can automatically deal with the config directory

--- setup.sh
+++ setup.sh
@@ -1,30 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-[ $# -gt 0 ] && destination=$1 || destination=$HOME
+[ $# -gt 0 ] && destinationDir=$1 || destinationDir=$HOME
 dotfilesDir=$(cd $(dirname $0); pwd)
 
-echo "Destination directory: $destination"
+echo "Destination directory: $destinationDir"
 
 cd $dotfilesDir
 
-ignores='`basename $0` README.md *~'
-for df in `ls -1`; do
-    # If the file is in the ignores list, then just skip the rest
-    [ `echo ${ignores} | grep -c "${df}"` -eq 0 ] || continue
+doLink() {
+    dfpath=$1
+    target=$2
+    df=`basename ${dfpath}`
 
-    # Output was is being linked
+    # Output what is being linked
     echo -n "Linking "
     [ -d ${df} ] && echo -n "directory" || echo -n "file"
     echo " ${df}"
 
-    target="${destination}/.${df}"
 
     ## If a file already exists with this name
     # If it's a symlink
     [ -L ${target} ] && {
         # Ignore if it's just a link to the respective dotfiles file. Ask to delete it otherwise
         foundTarget="`ls -la ${target} | awk -F'->' '{ print $2 }' | cut -d' ' -f2`"
-        [ $foundTarget = "${dotfilesDir}/${df}" ] && {
+        [ $foundTarget = "${dfpath}" ] && {
             echo "  Already exists"
             continue
         } || {
@@ -43,6 +42,23 @@ for df in `ls -1`; do
     [ -e ${target} ] && {
         echo "Did not create link for ${df}" 
     } || {
-        ln -s ${dotfilesDir}/${df} ${target}
+        #echo "$target" 
+        [ ! -d `dirname ${target}` ] && mkdir -p ${target}
+        ln -s ${dfpath} ${target}
     }
+}
+
+ignores='`basename $0` README.md *~'
+for df in `ls -1`; do
+    # If the file is in the ignores list, then just skip the rest
+    [ `echo ${ignores} | grep -c "${df}"` -eq 0 ] || continue
+
+    [ "$df" = "config" ] && {
+        echo "You need to deal with this"
+        #echo "ok: ${df}"
+        #for cf in `ls -1 ${df}`; do
+        #    echo "cf: $cf"
+        #    doLink ${dotfilesDir}/${df}/${cf} "${destinationDir}/.config/${cf}"
+        #done
+    } || doLink ${dotfilesDir}/${df} "${destinationDir}/.${df}"
 done
