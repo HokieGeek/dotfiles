78575b68833465a38b08489e1a1c1322ba95c150
HokieGeek <andres.f.perez@gmail.com>
2014-04-01 22:02:51 -0400

    Made a smarter update script (using ssh-agent) and updated vim plugins

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+[ `ps aux | grep ssh-agent | grep -v grep | wc -l` -le 0 ] && exec ssh-agent $0 $@
+ssh-add
+
 git fetch --all
 git pull origin master
-git submodule foreach --recursive 'git fetch --all && git pull origin master'
+cd vim/bundle
+git pull origin master
+./update.sh --mine
+./update.sh --theirs
