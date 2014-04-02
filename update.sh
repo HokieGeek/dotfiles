1f7386a8a5d9bbaa58718aa6e9f8f73973fcd375
HokieGeek <andres.f.perez@gmail.com>
2014-04-02 19:35:04 -0400

    A slightly better way of doing the ssh-agent check

--- update.sh
+++ update.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-[ `ps aux | grep ssh-agent | grep -v grep | wc -l` -le 0 ] && exec ssh-agent $0 $@
-# [ -z "$SSH_AGENT_PID" -o $PPID -ne $SSH_AGENT_PID ] && exec ssh-agent $0 $@
+# [ `ps aux | grep ssh-agent | grep -v grep | wc -l` -le 0 ] && exec ssh-agent $0 $@
+[ -z "$SSH_AGENT_PID" ] && exec ssh-agent $0 $@
 ssh-add
 
 git fetch --all
 git pull origin master
 cd vim/bundle
+git fetch --all
 git pull origin master
 ./update.sh --mine
 ./update.sh --theirs
