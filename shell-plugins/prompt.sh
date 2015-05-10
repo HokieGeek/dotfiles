8a8e320caf0b8194ce9d3ec8e99ded3117d89a0c
HokieGeek <andres.f.perez@gmail.com>
2015-05-10 10:52:21 -0400

    Making sure that bash stays somewhat up to date with the colors, I guess

--- shell-plugins/prompt.sh
+++ shell-plugins/prompt.sh
@@ -2,7 +2,7 @@
 
 prompt_gitInfo() {
     # echo -en ' \[\e[35m\]'
-    echo -n ' \[\e[01;36m\]'
+    echo -n ' \[\e[33m\]'
 
     # Put in the branch
     branch=`git branch | grep "^*" | sed "s/^\*\s*//"`
@@ -29,11 +29,11 @@ prompt_repoInfo() {
 }
 
 prompt_lastReturnVal() {
-    [ $1 -gt 0 ] && echo -n '\[\e[31m\]☹\[\e[0m\] '
+    [ $1 -gt 0 ] && echo -n '\[\e[31m\]∙\[\e[0m\]'
 }
 
 prompt_jobs() {
-    [ `jobs | wc -l` -gt 0 ] && echo -n '\[\e[01;31m\]¡\[\e[0m\] '
+    [ `jobs | wc -l` -gt 0 ] && echo -n '\[\e[31m\]∙\[\e[0m\]'
 }
 
 prompt_userHost() {
@@ -43,7 +43,7 @@ prompt_userHost() {
 prompt_pwd() {
     # echo -n '\[\e[33;1m\]\w\[\e[0m\]'
 
-    echo -n '\[\e[33;1m\]'
+    echo -n '\[\e[0;37;1m\]'
     top=$(dirname "$(dirname $HOME)")
     trueHome=`readlink $top``echo $HOME | sed "s;$top;;"`
     [ $(echo `pwd` | grep -c $trueHome) -gt 0 ] && echo -n $(echo `pwd` | sed "s;$trueHome;~;") || echo -n '\w'
@@ -51,7 +51,7 @@ prompt_pwd() {
 }
 
 prompt_display() {
-    echo 'val=$?;PS1="\[\e[37m\]╭\[\e[0m\] $(prompt_lastReturnVal $val)$(prompt_userHost)$(prompt_pwd)$(prompt_repoInfo)\n\[\e[37m\]╰─\[\e[0m\]$(prompt_jobs) "'
+    echo 'val=$?;PS1="\[\e[1;30m\]╭\[\e[0m\]$(prompt_jobs) $(prompt_userHost)$(prompt_pwd)$(prompt_repoInfo)\n\[\e[1;30m\]╰─\[\e[0m\]$(prompt_lastReturnVal $val) "'
 }
 PROMPT_COMMAND=$(prompt_display)
 # PROMPT_COMMAND='val=$?;PS1="$(prompt_jobs)$(prompt_userHost)$(prompt_pwd)$(prompt_repoInfo)\n$(prompt_lastReturnVal $val) "'
