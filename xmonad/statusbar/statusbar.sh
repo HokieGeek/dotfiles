be08fbdb02c0058cd1527a0c5078c0fe0696d376
HokieGeek <andres.f.perez@gmail.com>
2015-05-19 06:40:45 -0400

    Simplifying things

--- xmonad/statusbar/statusbar.sh
+++ xmonad/statusbar/statusbar.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
-here=$(cd $(dirname $0); pwd)
 
-${here}/statusbar.py > /tmp/xmonad.conkyrc && \
-    conky -b -c /tmp/xmonad.conkyrc | dzen2 \
-    -y '0' -x '0' -w `" ++ screenwidth_cmd ++ "` -h '" ++ barheight ++ \
-    "' -ta 'r' -bg '" ++ colorBackground ++ "' -fg '" ++ colorForeground ++ 
-    "' -fn '" ++ font ++ "'"
+# exec >> /tmp/xmonadstatusbar 2>&1
+# echo "$@"
+
+here=$(cd $(dirname $0); pwd)
+tmprc=/tmp/xmonad.conkyrc
+
+width=1
+height=1
+fg="#ffffff"
+bg="#000000"
+font="whatever"
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        --width) width=$2; shift ;;
+        --height) height=$2; shift ;;
+        --fg) fg=$2; shift ;;
+        --bg) bg=$2; shift ;;
+        --font) font=$2; shift ;;
+        # *) usage ;;
+    esac
+    shift
+done
+
+# set -x
+${here}/statusbar.py > ${tmprc} && {
+conky -b -c ${tmprc} | dzen2 -y '0' -x '0' -ta 'r' \
+    -w ${width} -h ${height} \
+    -bg "${bg}" -fg "${fg}" \
+    -fn "${font}"
+}
