b535a474c4617a9ae1a247271874d45322651a72
HokieGeek <andres.f.perez@gmail.com>
2015-05-23 13:49:50 -0400

    Smarter python pathing and going back to regular conky

--- xmonad/statusbar/statusbar.sh
+++ xmonad/statusbar/statusbar.sh
@@ -24,7 +24,6 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-# set -x
-#/usr/local/bin/python3.4 
-/usr/bin/python ${here}/statusbar.py --height ${height} --color-fg ${fg} --color-bg ${bg} > ${tmprc} || exit 1
+[ -f /usr/local/bin/python3.4 ] && py="/usr/local/bin/python3.4" || py=`which python`
+${py} ${here}/statusbar.py --height ${height} --color-fg ${fg} --color-bg ${bg} > ${tmprc} || exit 1
 conky -c ${tmprc} | dzen2 -y '0' -x '0' -ta 'r' -w ${width} -fn "${font}" -bg ${bg}
